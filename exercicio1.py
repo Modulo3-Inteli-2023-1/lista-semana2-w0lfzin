43cb7888172747aee39c095a1c56821e9dd7923b
w0lfzin <84356798+w0lfzin@users.noreply.github.com>
2023-08-14 03:06:41 -0300

    ex 1 e 2

--- exercicio1.py
+++ exercicio1.py
@@ -6,6 +6,16 @@
 
 # Crie a função que será avaliada no exercício aqui
 
+def conta_palavras_unicas(frase):
+    palavras = frase.lower().split()
+    
+    palavras_unicas = set(palavras)
+    
+    return len(palavras_unicas)
+
+frase = "Essa frase é um teste, essa frase possui palavras repetidas"
+quantidade = conta_palavras_unicas(frase)
+print("Quantidade de palavras únicas:", quantidade)
 
 
 
